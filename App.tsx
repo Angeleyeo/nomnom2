90be386575d6bab04eb58cb520e681658cf5b6fa
Angeleyeo <66460019+Angeleyeo@users.noreply.github.com>
2021-09-05 02:44:39 +0800

    Remove unnecessary fonts

--- App.tsx
+++ App.tsx
@@ -3,18 +3,6 @@ import Navigator from "./routes/routes.js";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
 
-const fetchFont = () => {
-  return Font.loadAsync({
-<<<<<<< HEAD
-    'Dancing-Script': require('./assets/fonts/DancingScript-VariableFont_wght.ttf')
-  })
-}
-=======
-    "Dancing-Script": require("./assets/fonts/DancingScript-VariableFont_wght.ttf"),
-  });
-};
->>>>>>> 9c2c557c518fdbcfbfeff4d9e21dfc45307bae38
-
 export default function App() {
   const [fontLoaded, setfontLoaded] = useState(false);
   /*if (!fontLoaded) {
